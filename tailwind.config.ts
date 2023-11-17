4b7bc8772df4ed4c8ca8b4ed5c17af044be03d97
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-17 10:00:08 -0300

    fix: arrumando as cores do tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,13 +6,16 @@ const config: Config = {
         "./app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
-        colors: {
-            highlight: "#EC3E17",
-            text: "#061721",
-            medium: "#174856",
-            "light-blue": "#84C6BF",
-            light: "#FBFBFF",
-        },
+        extend: {
+            colors: {
+                highlight: "#EC3E17",
+                text: "#061721",
+                medium: "#174856",
+                "light-blue": "#84C6BF",
+                light: "#FBFBFF",
+                border_color: "#608F9C"
+            },
+        }
     },
     plugins: [],
 };
