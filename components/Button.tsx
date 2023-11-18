34297151b3e75f5441e10cb9a00f5d948733b3f3
Roberto Menezes <roberto.menesantos@gmail.com>
2023-11-18 14:05:07 -0300

    feat: adicionando type, extraStyle na interface IButton e adicionando trackEmail e trackPass na interface IInputDefaultProps

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,9 +1,18 @@
-const Button = ({text}: {text: string}) => {
-    return (
-        <button className="w-[219px] h-[40px] active:scale-[0.95] bg-highlight rounded-md text-white font-bold">
-            {text}
-        </button>
-    )
+interface IButton {
+    type?: "button" | "reset" | "submit" | undefined;
+    text: string;
+    extraStyle?: string;
 }
 
-export default Button
+const Button = ({ type, text, extraStyle }: IButton) => {
+    return (
+        <button
+            type={type ?? "button"}
+            className={`w-[219px] h-[40px] active:scale-[0.95] bg-highlight rounded-md text-white font-bold ${extraStyle}`}
+        >
+            {text}
+        </button>
+    );
+};
+
+export default Button;
--- components/form/InputDefault.tsx
+++ components/form/InputDefault.tsx
@@ -1,23 +1,68 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 
-const InputDefault = ({text, type}: {text: string, type: string}) => {
-    const [seePassword, setSeePassword] = useState<boolean>(false)
-    
-    return (
-        <div className="w-full relative">
-            <label className="text-sm block" htmlFor="formInput">{text}</label>
-            <input 
-                id="formInput" 
-                type={type === "password" ? seePassword ? "text" : "password" : type}
-                required 
-                placeholder="Informe o seu email" 
-                className="p-2 w-full  text max-w-md border-border_color border-2 outline-none rounded-md"/>
-            {type === "password" && <input type="checkbox" defaultChecked={seePassword} onClick={() => setSeePassword(prev => prev === false ? true : false)}
-                className="absolute right-2 top-9" />}
-        </div>
-    )
+interface IInputDefaultProps {
+    text: string;
+    type: string;
+    trackEmail?: {
+        email: string;
+        setEmail: (string: string) => void;
+    };
+    trackPass?: {
+        password?: string;
+        setPassword?: (string: string) => void;
+    };
 }
 
-export default InputDefault
+const InputDefault = ({
+    text,
+    type,
+    trackPass,
+    trackEmail,
+}: IInputDefaultProps) => {
+    const [seePassword, setSeePassword] = useState<boolean>(false);
+
+    return (
+        <div className="w-full relative">
+            <label className="text-sm block" htmlFor="formInput">
+                {text}
+            </label>
+            <input
+                id="formInput"
+                type={
+                    type === "password"
+                        ? seePassword
+                            ? "text"
+                            : "password"
+                        : type
+                }
+                required
+                placeholder="Informe o seu email"
+                className="p-2 w-full  text max-w-md border-border_color border-2 outline-none rounded-md"
+                value={
+                    type === "text" ? trackEmail?.email : trackPass?.password
+                }
+                onChange={(event) => {
+                    type === "text"
+                        ? trackEmail?.setEmail(event.target.value)
+                        : trackPass?.setPassword!(event.target.value);
+                }}
+            />
+            {type === "password" && (
+                <input
+                    type="checkbox"
+                    defaultChecked={seePassword}
+                    onClick={() =>
+                        setSeePassword((prev) =>
+                            prev === false ? true : false
+                        )
+                    }
+                    className="absolute right-2 top-9"
+                />
+            )}
+        </div>
+    );
+};
+
+export default InputDefault;
